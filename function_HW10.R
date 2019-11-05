182ca5b5745ba8347cda66ef7e61a3ce9b7f6bd6
NamikoMachida <nm0076@mix.wvu.edu>
2019-11-05 16:37:11 -0500

    Solved steps 1, 2, and part of 3. Reached the point enabling users to enter the year in 'yyyy' format instead of 'Xyyyy'. Now working on reporting NAs and throwing errors.

---
+++ function_HW10.R
@@ -0,0 +1,36 @@
+co2_report <- function(Country, Year){
+
+# First, tailor the year format from 'yyyy' into 'Xyyyy'.  
+  Year <- c("X", Year)
+  XYear <- paste(Year, collapse = "")
+
+# Read the csv file and extract the target data from the file.  
+  emissions <- read.csv(file = "./data/co2_emissions_tonnes_per_person_gapminder.csv")
+  target <- emissions[emissions$country == Country, XYear]
+
+# Define quantile values (p10, p20 etc.) by using `quantile`.  
+  p10 = quantile(emissions[, XYear], 0.10)
+  p20 = quantile(emissions[, XYear], 0.20)
+  p30 = quantile(emissions[, XYear], 0.30)
+  p40 = quantile(emissions[, XYear], 0.40)
+
+# Put the target value (CO2 emission) into the grade categories.  
+  if (target <= p10){
+    grade <- "A"
+  } else if (target <= p20){
+    grade <- "B"
+  } else if (target <= p30){
+    grade <- "C"
+  } else if (target <= p40){
+    grade <- "D"
+  } else{
+    grade <- "F"
+  }
+
+# Report the above results in a human readable format.  
+  grade_report <- c("Grade:", grade, ";")
+  grade_sentence <- paste(grade_report, collapse = "")
+  emission_report <- c("tonnes", "CO2", "per", "person", "per", "year")
+  emission_sentence <- paste(emission_report, collapse = " ")
+  paste(grade_sentence, target, emission_sentence)
+}

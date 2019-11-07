264647b613316d192e6b2776769e98ec71b2ed8b
cmmalay <54336641+cmmalay@users.noreply.github.com>
2019-11-07 18:12:58 -0500

    Update function_HW10.R

--- function_HW10.R
+++ function_HW10.R
@@ -8,13 +8,13 @@ co2_report <- function(Country, Year){
   emissions <- read.csv(file = "./data/co2_emissions_tonnes_per_person_gapminder.csv")
   target <- emissions[emissions$country == Country, XYear]
 
-# Define quantile values (p10, p20 etc.) by using `quantile`.  
+# Define quantile values (p10, p20 etc.) by using `quantile` to retrieve the cutoffs for grades.  
   p10 = quantile(emissions[, XYear], 0.10)
   p20 = quantile(emissions[, XYear], 0.20)
   p30 = quantile(emissions[, XYear], 0.30)
   p40 = quantile(emissions[, XYear], 0.40)
 
-# Put the target value (CO2 emission) into the grade categories.  
+# Put the target values (CO2 emission) that were defined above into the grade categories.  
   if (target <= p10){
     grade <- "A"
   } else if (target <= p20){
@@ -27,7 +27,8 @@ co2_report <- function(Country, Year){
     grade <- "F"
   }
 
-# Report the above results in a human readable format.  
+# Report the above results in a human readable format. 
+# Example result: "7.4 tonnes CO2 per person per year"
   grade_report <- c("Grade:", grade, ";")
   grade_sentence <- paste(grade_report, collapse = "")
   emission_report <- c("tonnes", "CO2", "per", "person", "per", "year")
